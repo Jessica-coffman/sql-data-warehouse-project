3375141940ac9fced2fc542a53e5514eeecb1ee1
Jessica-coffman <jessicac667@outlook.com>
2025-10-18 19:58:30 -0400

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -52,6 +52,7 @@ BEGIN
         FROM bronze.crm_cust_info
         WHERE cst_id IS NOT NULL
     )t
+        WHERE flag_last = 1; -- Select the most recent record per customer
 		SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 
