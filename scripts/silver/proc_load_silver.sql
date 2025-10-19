fe8a7bb1dce454be0b6d17c5773e6e8956e3fd81
Jessica-coffman <jessicac667@outlook.com>
2025-10-18 21:55:44 -0400

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -74,7 +74,7 @@ BEGIN
     SELECT
         prd_id,
         REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
-        REPLACE(SUBSTRING(prd_key, 7, lEN(prd_key)), '-', '_') AS prd_key,
+        REPLACE(prd_key, 7, lEN(prd_key)) AS prd_key,
         prd_nm,
         ISNULL(prd_cost, 0) AS prd_cost,
         CASE UPPER(TRIM(prd_line))
@@ -83,7 +83,7 @@ BEGIN
              WHEN 'S' THEN 'other Sales'
              WHEN 'T' THEN 'Touring'
              ELSE 'n/a'
-        END prd_line,
+        END AS prd_line,
         CAST (prd_start_dt AS DATE) AS prd_start_dt,
         CAST(
             LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 
